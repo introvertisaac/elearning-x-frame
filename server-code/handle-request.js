c718b8585bfba65332c3e07ecc03610896310b21
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2024-05-13 14:02:04 +0300

    reverted to http

--- server-code/handle-request.js
+++ server-code/handle-request.js
@@ -1,10 +1,10 @@
-// const https = require("https"); // Import the 'https' module
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const fs = require("fs"); // Import the 'fs' module to read files
 const app = express();
-const port = 3000; // HTTPS default port
+const port = 3000;
+
+// const bypassAPILink = `http://77.37.51.164:4200/bypass/`;
 
 /**
  * Extract domain from a url
@@ -48,16 +48,6 @@ app.get("/bypass/*", async (req, res) => {
     }
 });
 
-// // Configuration for HTTPS server
-// const options = {
-//     key: fs.readFileSync('/etc/ssl/private/selfsigned.key'), // Path to your private key file
-//     cert: fs.readFileSync('/etc/ssl/certs/selfsigned.crt') // Path to your certificate file
-// };
-
-// Create an HTTPS server
-const server = https.createServer(options, app);
-
-// Start the server
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
