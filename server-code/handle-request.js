9e9eff9b7a644810041ac7d96ade01cc0a650251
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2024-05-13 17:53:19 +0300

    reverted to http

--- server-code/handle-request.js
+++ server-code/handle-request.js
@@ -1,25 +1,10 @@
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
 const axios = require("axios");
 const cors = require("cors");
-
 const app = express();
-const port = 443; // HTTPS default port
+const port = 3000;
 
-// Path to your SSL/TLS certificate and key files
-const certFilePath = '/etc/letsencrypt/live/code-org.smartbrainskenya.com/fullchain.pem';
-const keyFilePath = '/etc/letsencrypt/live/code-org.smartbrainskenya.com/privkey.pem';
-
-// Read the SSL/TLS certificate and key files
-const cert = fs.readFileSync(certFilePath);
-const key = fs.readFileSync(keyFilePath);
-
-// SSL/TLS certificate options
-const options = {
-    cert: cert,
-    key: key
-};
+// const bypassAPILink = `http://77.37.51.164:4200/bypass/`;
 
 /**
  * Extract domain from a url
@@ -28,7 +13,9 @@ const options = {
  * @returns
  */
 function extractDomain(url) {
-    const match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im);
+    const match = url.match(
+        /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im
+    );
     return match && match[0];
 }
 
@@ -61,10 +48,6 @@ app.get("/bypass/*", async (req, res) => {
     }
 });
 
-// Create an HTTPS server
-const server = https.createServer(options, app);
-
-// Start the server
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
