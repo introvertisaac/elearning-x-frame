bee29a46c8a001e95047aaf4ba45e8090e9987f2
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2024-05-13 16:50:48 +0300

    updated url to vps url

--- bypass.js
+++ bypass.js
@@ -1,5 +1,5 @@
 const iFrames = [...document.querySelectorAll("iframe[bypass-x-frame]")];
-const bypassAPILink = "http://localhost:3000/bypass/";
+const bypassAPILink = "http://77.37.51.164:3000/bypass/";
 
 iFrames.forEach(async (frame) => {
     let link = frame.getAttribute("bypass-x-frame");
